da7baeca8a57fc0bc6b7a319084cd73ebad8bca1
Steve Deemer <steve@capeandbay.com>
2024-05-03 10:02:58 -0400

    app.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,6 @@
       <script src="{{ asset('js/app.js') }}"></script>
    <!-- Add this in the <head> section -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
 <style>
         /* Custom CSS for sticky footer */
         body {
