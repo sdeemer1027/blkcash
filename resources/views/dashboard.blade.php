1ee3bc8b1e7fa3fdde1101e8352b9aef0dd4aa5d
Steve Deemer <steve@capeandbay.com>
2024-05-11 12:11:51 -0400

    update to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     {{-- 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -16,8 +18,13 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                   
-                    <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
-                    <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+                    <p>This is your dashboard</p>
+                    <p>I will be replacing this by redirecting you to the home page</p>
+                    <p>&nbsp;</p><p>
+                        Click the home button icon on the bottom to get there now or you can 
+                        <BR><a href="{{route('home')}}" class="btn btn-small btn-primary">Click here</a>
+
+                    </p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
                     <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p> {{ __("You're logged in!") }}
                 </div>
             </div>
