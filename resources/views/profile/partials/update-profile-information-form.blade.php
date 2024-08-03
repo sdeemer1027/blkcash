b3040664aeece3f8fbe1d6d6619181a50099b35f
Steve Deemer <it@xcsat.com>
2024-08-03 15:18:27 -0400

    sdsd

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -18,20 +18,27 @@
         @method('patch')
 
         <div>
+            <x-input-label for="validphone" :value="__('validphone')" />
+            <x-text-input id="validphone" validphone="name" type="text" class="mt-1 block w-full" :value="old('validphone', $user->validphone)"  />
+
+        </div>
+
+
+        <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
- <div>
+        <div>
             <x-input-label for="firstname" :value="__('First Name')" />
             <x-text-input id="firstname" name="firstname" type="text" class="mt-1 block w-full" :value="old('firstname', $user->firstname)" required/>
-          
+
         </div>
          <div>
             <x-input-label for="lastname" :value="__('Last Name')" />
             <x-text-input id="lastname" name="lastname" type="text" class="mt-1 block w-full" :value="old('lastname', $user->lastname)" required/>
-            
+
         </div>
 
 
@@ -43,7 +50,7 @@
     <div class="form-group">
          <x-input-label for="phone" :value="__('Phone')" />
             <x-text-input id="phone" name="phone" type="text" class="mt-1 block w-full" :value="old('phone', $user->phone)"  required />
-        
+
     </div>
         <div>
             <x-input-label for="email" :value="__('Email')" />
@@ -72,22 +79,22 @@
  <div class="form-group">
          <x-input-label for="address" :value="__('Adddress')" />
             <x-text-input id="address" name="address" type="text" class="mt-1 block w-full" :value="old('address', $user->address)"  required />
-        
+
     </div>
     <div class="form-group">
          <x-input-label for="city" :value="__('City')" />
             <x-text-input id="city" name="city" type="text" class="mt-1 block w-full" :value="old('city', $user->city)"   required/>
-        
+
     </div>
  <div class="form-group">
          <x-input-label for="state" :value="__('State')" />
             <x-text-input id="state" name="state" type="text" class="mt-1 block w-full" :value="old('state', $user->state)"  required  />
-        
+
     </div>
 <div class="form-group">
          <x-input-label for="zip" :value="__('Zip')" />
             <x-text-input id="zip" name="zip" type="text" class="mt-1 block w-full" :value="old('zip', $user->zip)"  required />
-        
+
     </div>
 
 
