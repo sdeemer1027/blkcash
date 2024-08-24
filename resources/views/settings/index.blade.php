4bcb0f2fc55a72357309ecb581429645ce719025
Steve Deemer <it@xcsat.com>
2024-08-24 12:10:22 -0400

    more

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -4,75 +4,50 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-{{--
-                    you are now using<BR>
-                    to Send :{{$user->snd}}
-                    @foreach($files as $img)
-                  @if($img->id == $user->snd)
-                    <img src="{{ asset($img->path . '/' . $img->name) }}" alt="{{ $img->name }}" width="100px">
-                        @endif
-                    @endforeach
-                    <BR>
-                    To Receive : {{$user->rcv}}
-                    @foreach($files as $img)
-                        @if($img->id == $user->rcv)
-                            <img src="{{ asset($img->path . '/' . $img->name) }}" alt="{{ $img->name }}" width="100px">
-                        @endif
-                    @endforeach
-                    <BR>
 
---}}
-
-                    <table  class="table">
+                    <table class="table">
+                        <thead>
                         <tr>
-                            <th>file</th>
-                            <th>send $</th>
-                            <th>Receive $</th>
+                            <th>File</th>
+                            <th>Send</th>
+                            <th>Receive</th>
                         </tr>
-                    @foreach($files as $img)
-<tr>
-                            <td>
-                                @if($img->id == $user->snd || $img->id == $user->rcv)
-                                    USED
-                                @else
-                        <img src="{{ asset($img->path . '/' . $img->name) }}" alt="{{ $img->name }}" width="100px">
-                                @endif
-                            </td>
+                        </thead>
+                        <tbody>
+                        @foreach($files as $img)
+                            <tr>
+                                <td>
+                                    @if($img->id == $user->snd || $img->id == $user->rcv)
+                                        USED
+                                    @else
+                                        <img src="{{ asset($img->path . '/' . $img->name) }}" alt="{{ $img->name }}" width="100px">
+                                    @endif
+                                </td>
                                 <td>
                                     @if($img->id == $user->snd)
                                         <img src="{{ asset($img->path . '/' . $img->name) }}" alt="{{ $img->name }}" width="100px">
-
                                     @else
-
-
-                                    <form action="{{ route('settings.update', ['id' => $user->id]) }}" method="POST">
-                                    @csrf
-                                                <input name="snd" type="hidden" value="{{ $img->id }}" {{ $img->id == $user->snd ? 'selected' : '' }} />
-                                          <div>
-                                            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">send with</button>
-                                        </div>
-                                    </form>
-                                        @endif
-
+                                        <form action="{{ route('settings.update', ['id' => $user->id]) }}" method="POST">
+                                            @csrf
+                                            <input name="snd" type="hidden" value="{{ $img->id }}" />
+                                            <button type="submit" class="btn btn-primary">Send with</button>
+                                        </form>
+                                    @endif
                                 </td>
                                 <td>
-
                                     @if($img->id == $user->rcv)
                                         <img src="{{ asset($img->path . '/' . $img->name) }}" alt="{{ $img->name }}" width="100px">
-
                                     @else
-
-                                    <form action="{{ route('settings.update', ['id' => $user->id]) }}" method="POST">
-                                        @csrf
-                                        <input name="rcv" type="hidden" value="{{ $img->id }}" {{ $img->id == $user->snd ? 'selected' : '' }} />
-                                        <div>
-                                            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">receive with</button>
-                                        </div>
-                                    </form>
-@endif
+                                        <form action="{{ route('settings.update', ['id' => $user->id]) }}" method="POST">
+                                            @csrf
+                                            <input name="rcv" type="hidden" value="{{ $img->id }}" />
+                                            <button type="submit" class="btn btn-primary">Receive with</button>
+                                        </form>
+                                    @endif
                                 </td>
                             </tr>
-                    @endforeach
+                        @endforeach
+                        </tbody>
                     </table>
                 </div>
             </div>
