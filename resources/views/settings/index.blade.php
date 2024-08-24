949ec019fd5af418f928c8b2390127438a0851e1
Steve Deemer <it@xcsat.com>
2024-08-24 11:16:31 -0400

    fix

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -3,10 +3,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+                    <table  class="table">
+                        <tr>
+                            <th>file</th>
+                            <th>send $</th>
+                            <th>Receive $</th>
+                        </tr>
                     @foreach($files as $img)
+<tr>
+                            <td>
                         <img src="{{ asset($img->path . '/' . $img->name) }}" alt="{{ $img->name }}" width="100px">
-                        <br>
+                            </td>
+                                <td> Use </td>
+                                <td> Use </td>
+                            </tr>
                     @endforeach
+                    </table>
                 </div>
             </div>
         </div>
