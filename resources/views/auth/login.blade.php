f7160d121a46387ddf11c9a2a7859f180e59faff
Steve Deemer <it@xcsat.com>
2024-10-05 11:51:30 -0400

    register button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
-                &nbsp;&nbsp;&nbsp; <a href="/resgister" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent
+                &nbsp;&nbsp;&nbsp; <a href="{{route('register')}}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent
             rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700
             active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
             transition ease-in-out duration-150 ms-3" style="float:left">Register</a>
