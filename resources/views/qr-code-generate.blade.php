7d0a459654cd82ac977621f2079b873eedd2648e
Steve Deemer <it@xcsat.com>
2024-10-05 17:25:21 -0400

    aligned

--- resources/views/qr-code-generate.blade.php
+++ resources/views/qr-code-generate.blade.php
@@ -4,23 +4,17 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-                    <h1>Your Generated QR Code</h1>
-                    <a href="{{route('qrcode.reader.form')}}" class="btn btn-danger btn-sm">Click here to scan a QRCode</a><br>
-                    <BR>
 
-                    {{--
-                                        <div class="container">
-                        <h1>QR Code Example</h1>
-                        {!! QrCode::size(300)->generate('1') !!}
+                    <div class="d-flex justify-content-center align-items-center  flex-column">
+                        <a href="{{ route('qrcode.reader.form') }}" class="btn btn-danger btn-sm mb-3">Click here to scan a QRCode</a>
+                        <h1>Your Generated QR Code</h1>
+
+
+                        {{-- QR Code Image --}}
+                        <img src="{{ $qrCode }}" alt="QR Code">
                     </div>
-
-                       --}}
-
-                    <img src="{{ $qrCode }}" alt="QR Code">
-
-
-
                 </div>
+
             </div>
         </div>
     </div>
