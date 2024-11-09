75c26985adf36f7c2df848e17703c30a14bd2676
Steve Deemer <it@xcsat.com>
2024-11-08 19:56:56 -0500

    tiny MCE

--- resources/views/content/edit.blade.php
+++ resources/views/content/edit.blade.php
@@ -6,17 +6,30 @@
                 <div class="p-6 text-gray-900">
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
                     <a href="{{route('home')}}">Home</a> -> <a href="{{route('content.show')}}">Content</a>
+                    <!-- In your Blade template file -->
+
+
+
+                    <script src="/js/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
+                    <script>
+                        tinymce.init({
+                            selector: '#editor',
+                            plugins: 'lists link image table code',
+                            toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | outdent indent',
+                            menubar: false
+                        });
+                    </script>
 
 
                     <form action="{{ route('content.update', $content->id) }}" method="POST">
     @csrf
     <label for="title">Title</label>
-    <input type="text" name="title" value="{{ $content->title }}">
+    <input type="text" name="title" value="{{ $content->title }}"><BR>
 
-    <label for="content">Content</label>
-    <textarea name="content" rows="10">{{ $content->content }}</textarea>
+    <label for="content">Content</label><BR>
+    <textarea id="editor" name="content" rows="10">{{ $content->content }}</textarea>
 
-    <button type="submit">Update</button>
+    <button type="submit" class="btn btn-primary">Update</button>
 </form>
 
 
